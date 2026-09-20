3d810697e3ed7017348c8d4591449c5d61c89903
Sunhik | AI Infrastructure Architect <47494378+Scarteam1@users.noreply.github.com>
2026-09-20 21:38:17 +0530

    Time: 0 ms (100%), Space: 19.7 MB (14.56%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.py
@@ -12,5 +12,6 @@ class Solution:
         if root.val == 2:
             return self.evaluateTree(root.left) or self.evaluateTree(root.right)
 
-        return self.evaluateTree(root.left) and self.evaluateTree(root.right)
+        if root.val == 3:
+            return self.evaluateTree(root.left) and self.evaluateTree(root.right)
         
